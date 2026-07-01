a02aa2b69bbf05c0b70897e37749df671140989c
Flesh2828 <hafidztaufip@gmail.com>
2026-07-01 20:23:59 +0700

    Bikin mentahan
    Bikin mentahan controller,view, ama model

---
+++ ProjekExpoKel1MoBis/ProjekEXPOKEL1MoBis/src/HomePageController.java
@@ -0,0 +1,23 @@
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.collections.ObservableList;
+import javafx.collections.FXCollections;
+
+public class HomePageController implements   Initializable {
+
+
+
+
+
+
+    
+  @Override
+  public void initialize(URL url, ResourceBundle rb) {
+    }
+
+}
---
+++ ProjekExpoKel1MoBis/ProjekEXPOKEL1MoBis/src/ModelMoBis.java
@@ -0,0 +1,20 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+
+public class ModelMoBis extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("HomePageMoBis.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("Model MoBis");
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
