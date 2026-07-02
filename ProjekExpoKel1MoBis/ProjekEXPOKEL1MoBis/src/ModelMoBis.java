8fbc42f98000f3066d3343c4857bb2a37298cc1d
Flesh2828 <hafidztaufip@gmail.com>
2026-07-02 12:31:01 +0700

    ganti nama jadi dashboard

--- ProjekExpoKel1MoBis/ProjekEXPOKEL1MoBis/src/ModelMoBis.java
+++ ProjekExpoKel1MoBis/ProjekEXPOKEL1MoBis/src/ModelMoBis.java
@@ -7,7 +7,7 @@ import javafx.scene.Parent;
 public class ModelMoBis extends Application {
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("HomePageMoBis.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("Dashboard.fxml"));
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.setTitle("Model MoBis");
